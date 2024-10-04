1aacd25de4e7d100daea359f7e6bf6fdd6c7b85b
Paherrerac <paherrerac@udistrital.edu.co>
2024-10-03 22:30:02 -0500

    Update operaciones.py

--- Corte 1/Calculadora/operaciones.py
+++ Corte 1/Calculadora/operaciones.py
@@ -1,1 +1,36 @@
+class Menu:
+    def showOptions(self):
+        print("\n--- Calculadora ---")
+        print("1. Suma")
+        print("2. Resta")
+        print("3. Multiplicación")
+        print("4. División")
+      
+    def chooseOption(self):
+        option = int(input("Seleccione la operación: "))
+        return option
+
+class Enter_Data:
+    def enterA(self):
+        a = int(input("Digite un valor: "))
+        return a
+    
+    def enterB(self):
+        b = int(input("Digite otro valor: "))
+        return b
+    
+class Add_Sub:
+    def add(self, a, b):
+        return a+b
+    
+    def sub(self, a, b):
+        return a-b
+
+class Mult_Div:
+    def mult(self, a, b):
+        return a*b
+    
+    def div(self, a, b):
+        return a/b
+    
 
